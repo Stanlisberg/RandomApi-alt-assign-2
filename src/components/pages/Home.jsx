5aa1982e8426c89c3437a3b72d638ee0edc781f6
Stanlisberg <nnabugo2010@gmail.com>
2022-10-29 14:40:33 -0700

    Refactor

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,32 +3,18 @@ import React from "react";
 function Home() {
   return (
     <div className="home-container">
-       <div className='home-header'>
-         <div className='overview'>Overview:</div>
-         <div className="home-wrapper">Random<span style={{color: "teal"}}>Api</span></div>
-       </div>
-         <div className="home-content-div">
-          <p className='home-content'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique qui
-          dolorum aspernatur quibusdam alias odio, animi perferendis blanditiis
-          ratione doloribus corrupti atque provident tenetur, deleniti laudantium
-          quam non. Nam placeat neque rem numquam, mollitia maiores impedit dolor
-          suscipit eius velit quibusdam eligendi excepturi necessitatibus? Ipsa
-          aut ea vero fugiat Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Nesciunt culpa sequi tenetur eos qui sunt, dicta praesentium
-          excepturi eum? Quisquam, deserunt. Dolorum et fugit cupiditate! Et ut
-          adipisci at voluptas, dolor ratione odit ex temporibus officiis
-          doloremque labore illo. Voluptate dolores est aliquam nesciunt optio
-          reiciendis at reprehenderit nulla maiores. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Recusandae mollitia dolor qui. Repudiandae
-          saepe ut quibusdam ipsa dignissimos omnis, in voluptatibus nobis tempore
-          perspiciatis est quas voluptatum numquam illo! Recusandae, magnam harum
-          neque laboriosam eos repellat corporis. Quam fugiat fugit, animi soluta,
-          veritatis praesentium recusandae exercitationem aut alias error dolore.
+      <div className="home-wrapper">
+        <div className="home-header">
+          <div className="overview">Overview:</div>
+        </div>
+        <div className="home-content-div">
+          <p className="home-content">
+            Fetch Random users using the Random users Api.
           </p>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
