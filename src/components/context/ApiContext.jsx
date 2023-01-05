9cf6f74536a3636668cab4fe0657294b75f36471
Stanlisberg <nnabugo2010@gmail.com>
2023-01-06 00:08:28 +0100

    changes

--- src/components/context/ApiContext.jsx
+++ src/components/context/ApiContext.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
-import { useState, createContext} from 'react'
+import { useState, createContext } from 'react'
 
 const ApiContext = createContext();
- 
-export const ApiProvider = ({children}) => {
+
+export const ApiProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
     const [user, setUser] = useState([])
- 
+
     const fetchApiData = async () => {
         const response = await fetch('https://randomuser.me/api?results=100');
         const { results } = await response.json()
         console.log(results)
-         setUser(results)
-         setLoading(false);
-        // }
-       }
-   
-    return(  
+        setUser(results)
+        setLoading(false);
+
+    }
+
+    return (
         <ApiContext.Provider value={{
             user,
             loading,
             fetchApiData,
         }}>
-        {children}
+            {children}
         </ApiContext.Provider>
     );
 };
